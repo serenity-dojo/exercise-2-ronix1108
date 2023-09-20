a7c0ed9ff83e859e7a7fe581173f2306768fff0a
ronix1108 <145560370+ronix1108@users.noreply.github.com>
2023-09-20 19:38:27 +0300

    Add files via upload

---
+++ src/test/java/com/serenitydojo/WhenWorkingWithStrings.java
@@ -0,0 +1,73 @@
+package com.serenitydojo.datatypes;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class WhenWorkingWithStrings {
+
+    @Test
+    public void convertToLowerCase() {
+        String bookTitle = "The Cat In The Hat";
+        bookTitle= bookTitle.toLowerCase();
+
+        String lowerCaseTitle = bookTitle;
+
+        // TODO: Convert the book title to lower case and assign it to the lowerCaseTitle variable
+
+        assertThat(lowerCaseTitle, equalTo("the cat in the hat"));
+
+    }
+
+    @Test
+    public void convertToUpperCase() {
+        String bookTitle = "The Cat In The Hat";
+
+        bookTitle = bookTitle.toUpperCase();
+
+        String upperCaseTitle = bookTitle;
+        System.out.println(bookTitle);
+        // TODO: Convert the book title to upper case and assign it to the lowerCaseTitle variable
+
+        assertThat(upperCaseTitle, equalTo("THE CAT IN THE HAT"));
+    }
+
+    @Test
+    public void trimExtraSpaces() {
+        String bookTitle = "  The Cat In The Hat    ";
+
+        bookTitle= bookTitle.trim();
+
+        String trimmedTitle = bookTitle;
+        System.out.println(bookTitle);
+        // TODO: Trim the spaces before and after the title text
+
+        assertThat(trimmedTitle, equalTo("The Cat In The Hat"));
+    }
+
+    @Test
+    public void findTheLengthOfAString() {
+        String bookTitle = "The Cat In The Hat";
+
+        int length = 0;
+
+        length = bookTitle.length();
+        System.out.println(length);
+        // TODO: Find the number of characters in the string
+
+        assertThat(length, equalTo(18));
+    }
+
+    @Test
+    public void replacingAText() {
+        String bookTitle = "The Cat In The Hat";
+
+        bookTitle = bookTitle.replace("Cat", "Dog");
+
+        String updatedTitle = bookTitle;
+        // TODO: Replace the word "Cat" with "Dog
+
+        assertThat(updatedTitle, equalTo("The Dog In The Hat"));
+    }
+}
