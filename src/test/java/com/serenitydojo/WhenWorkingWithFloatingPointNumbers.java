2e3ac53efb290257212f64c487a5435f071e73f3
ronix1108 <145560370+ronix1108@users.noreply.github.com>
2023-09-20 20:02:20 +0300

    Add files via upload

---
+++ src/test/java/com/serenitydojo/WhenWorkingWithFloatingPointNumbers.java
@@ -0,0 +1,49 @@
+package com.serenitydojo.datatypes;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class WhenWorkingWithFloatingPointNumbers {
+
+    //
+    // Exercise 1: Convert 27 degrees Celcius to Farenheit
+    // The equation is: farenheit = (9 / 5) * celcius + 32
+    //
+    @Test
+    public void convertToFarenheit() {
+        double celcius = 27.0d;
+        double farenheit = 0.0d;
+        double calc = 1.8;
+
+        farenheit = calc * celcius + 32;
+        System.out.println("farenheit is " + farenheit);
+
+
+
+        // TODO: Use a floating point calculation to calculate the farenheit equivalent of the celcius value.
+
+        assertThat(farenheit, equalTo(80.6));
+    }
+
+    //
+    // Exercise 2: Convert kilograms to pounds
+    // There are 2.20462 pounds in a kilogram
+    //
+    @Test
+    public void convertMetersToFeet() {
+        int weightInKilograms = 50;
+        double weightInPounds = 0;
+        double conv = 2.20462d;
+
+        weightInPounds = weightInKilograms * conv;
+
+        System.out.println("Weight in pounds is " + weightInPounds);
+
+        // TODO: Use a floating point calculation to calculate the correct weight in pounds
+
+        assertThat(weightInPounds, equalTo(110.231));
+
+    }
+}
